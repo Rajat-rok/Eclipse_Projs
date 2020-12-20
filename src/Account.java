204d62ed243189ba3ca07891c89598687cc227d0
Rajat <Rajatrokaderjt@gmail.com>
2020-12-20 10:05:11 +0530

    sdf

--- src/Account.java
+++ src/Account.java
@@ -1,10 +1,32 @@
-
 public class Account {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("wdj");
-
+	int balance,accNo;
+	String name;
+	public Account(int balance, int accNo, String name) {
+		super();
+		this.balance = balance;
+		this.accNo = accNo;
+		this.name = name;
 	}
+    public void deposit(int depoAmt) {
+        this.balance += depoAmt;
+        System.out.println("Deposit of " + depoAmt + " New Bal is " + this.balance);
+    	}
+    @Override
+    public String toString() {
+        return "AccountInfo{" +
+                "name='" + name + '\'' +
+                ", Ac.number='" + accNo + '\'' +
+                ", bal=" + balance +
+                '}';
+    }
+	public int getBalance() {
+		System.out.println("Balance= " + balance);
+		return balance;
+	}
+	public int getAccNo() {
+		System.out.println("Acc. No. = " + accNo);
+		return accNo;
+	}
+	
 
 }
